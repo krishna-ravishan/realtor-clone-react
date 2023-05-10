38669c8054789f579caae7c2453c9eb95fdb5d2c
Krishna Ravishanker <126204661+krishna-ravishan@users.noreply.github.com>
2023-05-10 11:35:53 +0530

    Create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/home";
+import Offers from "./pages/offers";
+import Profile from "./pages/profile";
+import Register from "./pages/register";
+import SignIn from "./pages/signin";
+import ForgotPassword from "./pages/forgotpass";
+
 function App() {
-  return (
+  return ( 
     <>
-      <h1>Hello World!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/pages/forgotpass.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function forgotpass() {
+  return (
+    <div>Forgot Password</div>
+  )
+}
